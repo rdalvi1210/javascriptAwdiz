6e0c409a289b55500e02d070e48669435a555ceb
Ramchandra Dalvi <rdalvi1210@gmail.com>
2025-06-26 00:10:48 +0530

    Javasscript Day 2

---
+++ Day-1/index.js
@@ -0,0 +1,48 @@
+
+console.log(1, "Hello", "1");
+console.log(1, typeof 1);
+
+
+Boolean
+console.log(true, typeof true);
+
+
+var userName = "Awdiz";
+
+var userAge = 25;
+
+var isUserCheck = false;
+
+console.log(userAge, userName, isUserCheck);
+
+
+var counter1 = 10;
+var counter2 = 20;
+var result = counter1 + counter2
+console.log(result);
+
+
+var counter = 1;
+counter = counter + 1;
+counter++;
+counter += 1
+console.log(counter);
+
+
+var counter = 1;
+counter = counter - 1;
+counter--;
+counter -= 10;
+console.log(counter);
+
+
+{
+  const age = 20 ;
+  {
+    {
+      console.log(age);
+    }
+    console.log(age);
+  }
+  console.log(age);
+}
---
+++ Day-2/index.js
@@ -0,0 +1,37 @@
+// Operators
+
+// ==
+console.log("10" == 2);
+console.log("10" == "10");
+console.log("10" == 10);
+
+//===
+console.log("10" === 2);
+console.log("10" === 10);
+console.log(10 === 10);
+
+// > greater than
+console.log(2 > 1);
+console.log(2 > 10);
+
+// <  lesser than
+console.log(2 < 1);
+console.log(2 < 10);
+
+//greater than equal
+console.log(2 >= 2);
+console.log(20 >= 21);
+console.log(20 >= 10);
+
+//lesser than equal
+console.log(2 <= 2);
+console.log(20 <= 19);
+console.log(20 <= 21);
+
+// AND operator
+console.log(2 > 1 && 3 > 2 && 4 > 3 && 5 > 4);
+console.log(2 < 1 && 3 > 2 && 4 > 3 && 5 > 4);
+
+// OR operator
+console.log(2 > 10 || 3 > 20 || 2 < 10);
+console.log(2 > 10 || 3 > 20 || 2 > 10);
